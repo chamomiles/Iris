91b25354d8af89687ee79513ff05fdb52fd04891
chamomiles <tima.chamomile@gmail.com>
2022-07-19 15:27:09 +0300

    Initial Commit

---
+++ Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by Chamomile on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
